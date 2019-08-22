7e231e5eedde276491ce8f236d38711deb420315
DavidLBallard <email_id>
2019-08-22 11:19:55 -0500

    Fixed bugs,  running on heroku

--- app.py
+++ app.py
@@ -12,20 +12,24 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qtjcskcqjazqgl:c8c02a6072f642e71a3a63a91675cad427979ee4c8a41695368d281ae59a1082@ec2-23-23-182-18.compute-1.amazonaws.com:5432/dftcr4fl973q8v'
     app.debug = False
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'= False
-db= SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
 class Feedback(db.Model):
-    __tablename__= 'feedback'
-    id= db.Column(db.Integer, primary_key=True)
-    customer= db.Column(db.String(200), unique=True)
-    dealer= db.Column(db.String(200))
-    rating= db.Column(db.Integer)
-    comments= db.Column(db.Text())
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
     def __init__(self, customer, dealer, rating, comments):
-        self.customer= customer
-        self.dealer= dealer
-        self.rating= rating
-        self.comments= comments
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
 
 @app.route('/')
 def index():
@@ -35,22 +39,23 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        customer= request.form['customer']
-        dealer= request.form['dealer']
-        rating= request.form['rating']
-        comments= request.form['comments']
-        print(customer, dealer, rating, comments)
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        #print(customer, dealer, rating, comments)
         if customer == '' or dealer == '':
             return render_template('index.html', message='Please enter required fields.')
 
         if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
-            data= Feedback(customer, dealer, rating, comments)
+            data = Feedback(customer, dealer, rating, comments)
             db.session.add(data)
             db.session.commit()
             send_mail(customer, dealer, raiting, comments)
             return render_template('success.html')
 
         return render_template('index.html', message='You have already submitted feedback.')
+
 
 if __name__ == '__main__':
     app.run()
